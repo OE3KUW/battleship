9fec3f1e2ac863e4758bcc1fd61103db404b500b
OE3KUW <wolfgang.kuran@htlstp.ac.at>
2024-10-22 17:51:42 +0200

    main.c added

---
+++ main.c
@@ -0,0 +1,56 @@
+// ***************************************************************
+// battle Ship
+// ***************************************************************
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "console.h"
+
+/// Funktion, die das 2D-Array empfängt und es ausgibt
+void printArray(int **array, int rows, int cols);
+
+void printArray(int **array, int rows, int cols)
+{
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", array[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int rows = 3;
+    int cols = 4;
+
+    initConsole();
+
+    // Speicher für ein 2D-Array allokieren
+    int **array = (int **)malloc(rows * sizeof(int *));
+    for (int i = 0; i < rows; i++) {
+        array[i] = (int *)malloc(cols * sizeof(int));
+    }
+
+    // Das Array mit Werten füllen
+    int value = 1;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            array[i][j] = value++;
+        }
+    }
+
+    // Übergabe des Arrays an eine Funktion
+    printf("Array Inhalt:\n");
+    printArray(array, rows, cols);
+
+    getch();
+    // Speicher freigeben
+    for (int i = 0; i < rows; i++) {
+        free(array[i]);
+    }
+    free(array);
+    closeConsole();
+
+    return 0;
+}
